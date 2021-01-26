d83659b829ba22971a01c3333977f0ccd0c27e1e
Glenn Bryant <glennbryant3@gmail.com>
2021-01-26 20:38:11 +0900

    added validations to models

--- app/models/translator.rb
+++ app/models/translator.rb
@@ -1,3 +1,7 @@
 class Translator < ApplicationRecord
   belongs_to :user
+  validates :languages, presence: true
+  validates :description, presence: true
+  validates :availability, presence: true
+  validates :rates, presence: true
 end
