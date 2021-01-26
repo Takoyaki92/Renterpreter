6588c1219288decdcb2aca2a2bc33d1eb0a1fb6c
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-01-26 22:33:14 +0900

    translator model & user model updated with has_many bookings...ect

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings, dependent: :destroy
+  has_many :translators, through: :bookings
 end
